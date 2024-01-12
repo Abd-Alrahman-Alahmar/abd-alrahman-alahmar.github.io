29d07a347f8de272a0da5607a902679733fab187
Abd Alrahman Alahmar <abdalrahman.alahmar@student.griffith.ie>
2024-01-12 00:40:21 +0000

    Update Content

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -54,7 +54,7 @@ const projectsData = [
     id: 6,
     title: " ApartHotel",
     description: "Reservation System",
-    image: "/images/projects/apartHotel.jpg",
+    image: "/images/projects/aparthotel.jpg",
     tag: ["All", "Web"],
     gitUrl: "https://apart-hotel.vercel.app/",
     previewUrl: "/",
